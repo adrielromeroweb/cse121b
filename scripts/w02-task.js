802e03d0eeb4387a728e0ee47aaba8067f92005a
adrielromeroweb <marc.97.37@gmail.com>
2023-09-28 23:01:35 -0300

    progamming-task

---
+++ scripts/w02-task.js
@@ -0,0 +1,44 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Adriel Romero";
+
+const currentYear = 2023;
+
+const profilePicture = "images/me.jpg";
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+
+const foodElement = document.getElementById('food');
+
+const yearElement = document.querySelector('#year');
+
+const imageElement = document.querySelector('img');
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+yearElement.textContent = currentYear;
+
+imageElement.setAttribute('src', profilePicture);
+
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+/* Step 5 - Array */
+let foodArray = ['milanesa', 'hamburguesa', 'fideos', 'guiso'];
+
+foodElement.innerHTML = foodArray;
+
+let singleFood = ['banana'];
+
+foodArray.push(singleFood);
+
+foodElement.innerHTML += `<br>${foodArray}`;
+
+foodArray.shift();
+
+foodElement.innerHTML += `<br>${foodArray}`;
+
+foodArray.pop();
+
+foodElement.innerHTML += `<br>${foodArray}`;
